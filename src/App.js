1837026e0ef4a61c5b8cd32889c94b60bf1c7389
Victor <victor-limaa@hotmail.com>
2022-09-28 23:58:03 -0300

    fix route and api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Home from './pages/Home'
-import Car from './pages/Car'
-import Newcar from './pages/Newcar'
-
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Car from "./pages/Car";
+import Newcar from "./pages/Newcar";
 
 function App() {
-
-  
   return (
     <Router>
       <Switch>
-
-        <Route path='/' exact component={Login} />
-        <Route path='/home'
-          component={localStorage.getItem('authorization-token') ? Home : Login }/>
-          <Route path='/home/:placa'
-          component={localStorage.getItem('authorization-token') ? Home : Login }/>
-        <Route path='/newcar'
-          component={Newcar} />
-        <Route path='/:id'
-          component={localStorage.getItem('authorization-token') ? Car : Login} />
-          
-
+        <Route path="/" exact component={Login} />
+        <Route path="/home" component={Home} />
+        <Route path="/home/:placa" component={Home} />
+        <Route path="/newcar" component={Newcar} />
+        <Route path="/:id" component={Car} />
       </Switch>
     </Router>
   );
